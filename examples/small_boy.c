314766b55ad2b1290b22862d58939064807f387f
Gregor Potočnik <gregor.pot.gm@gmail.com>
2025-09-13 22:04:51 +0200

    Updated examples

--- examples/small_boy.c
+++ examples/small_boy.c
@@ -1,10 +1,8 @@
 static uint16_t small_boy_mem[] = {
-	0b0000000000000001, //ldi r0, 1
-	0b1000000000111111, //mv r1, pc
-	0b1000010000100000, //add r1, r0
-	0b1111110000000000, //flag
-	0b1010010000100000  //jmp r1
+	0b1000000000011111, // mv r0, pc
+	0b1111110000000000, // 	flag
+	0b1010010000000000, // 	jmp r0
+
 };
-static uint16_t small_boy_size = 5;
+static uint16_t small_boy_size = 3;
 static uint16_t small_boy_offset = 69;
-	
